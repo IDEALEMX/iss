ba170b7ae3d8b1b8fa0e168d0386ab8ec5953c67
IDEALEMX <lnxp5acnt@gmail.com>
2025-05-08 02:05:48 -0600

    mostly saving in case something goes wrong

--- src/main.c
+++ src/main.c
@@ -14,7 +14,14 @@
 // global variables
 char g_current_directory [2048] = "/home/ideale/";
 char g_paths [256][256];
-int number_of_paths;
+int g_number_of_paths = 0;
+
+void load_paths() {
+    strcpy(g_paths[0], "/home/ideale/scripts/");
+    strcpy(g_paths[1], "/bin/");
+
+    g_number_of_paths = 2;
+}
 
 void print_prompt() {
     char username [256];
@@ -23,7 +30,10 @@ void print_prompt() {
     char hostname [1024];
     gethostname(hostname, 1024);
 
-    printf("[%s@%s]%s: ", username, hostname, g_current_directory);
+    char current_dir [2048];
+    getcwd(current_dir, 2048);
+
+    printf("[%s@%s]%s: ", username, hostname, current_dir);
 }
 
 int is_file(const char *path) {
@@ -38,7 +48,7 @@ int is_dir(const char *path) {
     return S_ISDIR(path_stat.st_mode);
 }
 
-void execute_program(char program_path [2048]) {
+void execute_program(char program_path [2048], char arg1 [256], char arg2 [256], char arg3 [256], char arg4 [256]) {
     int rc = fork();
 
     // fork failed
@@ -46,7 +56,7 @@ void execute_program(char program_path [2048]) {
         printf("iss: program failed to execute! \n");
         // child process
     } else if (rc == 0) {
-        execl(program_path, "foo");
+        execl(program_path, arg1, arg2, arg3, arg4);
         // parent process
     } else {
         int status;
@@ -54,12 +64,12 @@ void execute_program(char program_path [2048]) {
     }
 }
 
-void process_command(char command [256]) {
+void process_command(char command [256], char arg1 [256], char arg2 [256], char arg3 [256], char arg4 [256]) {
     char first_character = command[0];
 
     // handdle full path
     if(first_character == '/') {
-        execute_program(command);
+        execute_program(command, arg1, arg2, arg3, arg4);
         return;
     }
 
@@ -74,7 +84,7 @@ void process_command(char command [256]) {
 
         strcat(buffer_string, &command[2]);
 
-        execute_program(buffer_string);
+        execute_program(buffer_string, arg1, arg2, arg3, arg4);
         return;
     }
 
@@ -85,7 +95,7 @@ void process_command(char command [256]) {
         strcpy(buffer_string, g_current_directory);
         strcat(buffer_string, &command[2]);
 
-        execute_program(buffer_string);
+        execute_program(buffer_string, arg1, arg2, arg3, arg4);
         return;
     }
 
@@ -98,14 +108,39 @@ void process_command(char command [256]) {
         strcat(buffer_string, "../");
         strcat(buffer_string, &command[2]);
 
-        execute_program(buffer_string);
+        execute_program(buffer_string, arg1, arg2, arg3, arg4);
         return;
     }
+
+    // throw error if there are no path variables
+    if (g_number_of_paths == 0) {
+        fprintf(stderr, "iss: no path variables! \n");
+        return;
+    }
+
+    // look for command in path variables
+    for (int i = 0; i < g_number_of_paths; i++) {
+        FILE *file;
+        char file_buffer [512];
+        strcpy(file_buffer, g_paths[i]);
+        strcat(file_buffer, command);
+
+        // printf("attempting: %s \n", file_buffer);
+
+        if((file = fopen(file_buffer,"r")) != NULL) {
+            execute_program(file_buffer, arg1, arg2, arg3, arg4);
+            return;
+        } 
+    }
+        
+    fprintf(stderr, "iss: command not found! \n");
 
 }
 
 // contains the main program loop
 int main() {
+    load_paths();
+
     while(true) {
         print_prompt();
         char user_input [1024];
@@ -118,10 +153,7 @@ int main() {
         if (word_number == 1 && word_array[0][0] == '\0')
             continue;
 
-        // testing execution
-
-        //char test_path [2048] = "/home/ideale/cProjects/linkedList/test";
-        process_command(word_array[0]);
+        process_command(word_array[0], word_array[1], word_array[2], word_array[3], word_array[4]);
     }
 }
 
